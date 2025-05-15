5300234778678eee951cceab3bac05d98f29003f
SignageHub <nyolastanley70@gmail.com>
2025-05-15 14:40:55 +0300

    Add files via upload

---
+++ Signage Hub.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+L_R = input("Login Or Register")
+if L_R == "Login":
+username = input("Enter Your username")
+Password = input("Enter your password")
+
+elif L_R == "Register":
+username = input("Enter Your username")
+Password = input("Enter your password")
+
+else:
+    print("Wrong Option")
